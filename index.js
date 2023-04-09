58362c3f1993442c474ef1f805f274ad3e3e9bee
sukhidoo <101160031+sukhidoo@users.noreply.github.com>
2023-04-09 01:00:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,51 +2,78 @@ let dragged = null;
 
 const toggleButton = document.getElementById("toggle-button");
 const container = document.querySelector("#container");
-const container2 = document.querySelector("#container2");
 
 toggleButton.addEventListener("click", function() {
   if (container.classList.contains("hidden")) {
     container.classList.remove("hidden");
-    container2.classList.remove("hidden");
   } else {
     container.classList.add("hidden");
-    container2.classList.add("hidden");
   }
 });
 
-// Initialize variables
 let currentImageIndex = 1;
-const maxImageIndex = 12;
+const maxImageIndex = 11;
 
-// Get DOM elements
 const nextImageButton = document.querySelector("#next-image-button");
+const prevImageButton = document.querySelector("#prev-image-button");
 const tokenContainer = document.querySelector("#round-token-container");
 
-// Set up click event listener for button
 nextImageButton.addEventListener("click", () => {
-  // Increment current image index
   currentImageIndex++;
 
-  // If we have reached the maximum image index, disable the button
   if (currentImageIndex > maxImageIndex) {
     nextImageButton.disabled = true;
   }
 
-  // Update image source
+  prevImageButton.disabled = false;
+
   const imagePath = `./images/${currentImageIndex}.png`;
   tokenContainer.querySelector(".token").src = imagePath;
 });
 
+prevImageButton.addEventListener("click", () => {
+  currentImageIndex--;
 
+  if (currentImageIndex === 1) {
+    prevImageButton.disabled = true;
+  }
 
+  nextImageButton.disabled = false;
+
+  const imagePath = `./images/${currentImageIndex}.png`;
+  tokenContainer.querySelector(".token").src = imagePath;
+});
+
+const disableSquaresButton = document.querySelector("#hide-black-squares");
+
+disableSquaresButton.addEventListener("click", function() {
+  const positionContainers = document.querySelectorAll(".position");
+  positionContainers.forEach(position => {
+    if (position.classList.contains("position-no-border")) {
+      position.classList.remove("position-no-border");
+    } else {
+      position.classList.add("position-no-border");
+    }
+  });
+});
+  
 document.addEventListener("dragstart", function(event) {
   const tokenContainer = event.target.closest(".token-container");
   
   if (tokenContainer) {
     dragged = tokenContainer;
     const tokenName = dragged.querySelector(".token-name").innerText;
+    const id = `token-${Date.now()}`;
+    if (dragged.classList.contains("repeatable")) {
+      dragged.setAttribute("id", id);
+    }
+    event.dataTransfer.setData("text/plain", id);
+    console.log("dragstart event triggered");
+    console.log("Dragged token:", tokenName);
+    console.log("Dragged token container:", dragged);
   }
 });
+
 
 document.addEventListener("dragover", function(event) {
   event.preventDefault();
@@ -54,37 +81,90 @@ document.addEventListener("dragover", function(event) {
 
 document.addEventListener("drop", function(event) {
   event.preventDefault();
-  
-  if (event.target.classList.contains("position")) {
-    const dropTarget = event.target;
-    const containerRect = dropTarget.getBoundingClientRect();
-    const draggedRect = dragged.getBoundingClientRect();
-    const offsetX = draggedRect.left - containerRect.left;
-    const offsetY = draggedRect.top - containerRect.top;
-    
+  const target = event.target;
+  console.log("drop event triggered");
+  console.log("Dropped into element:", target);
+
+  if (target.hasAttribute("id") && target.getAttribute("id") === "container") {
+    if (dragged.classList.contains("repeatable")) {
+      return;
+    }
+    const tokenContainers = target.querySelectorAll(".token-container");
+    let lastTokenContainer;
+    for (let i = tokenContainers.length - 1; i >= 0; i--) {
+      if (!tokenContainers[i].classList.contains("repeatable")) {
+        lastTokenContainer = tokenContainers[i];
+        break;
+      }
+    }
+    if (lastTokenContainer) {
+      const bossTokenContainer = target.querySelector(".repeatable");
+      const referenceNode = bossTokenContainer ? bossTokenContainer : null;
+
+      // Get the original index of the token within the container div
+      const originalIndex = dragged.getAttribute("data-original-index");
+      const newIndex = Array.from(lastTokenContainer.parentNode.children).indexOf(lastTokenContainer) + 1;
+      if (newIndex < originalIndex) {
+        lastTokenContainer.insertAdjacentElement("beforebegin", dragged);
+      } else {
+        lastTokenContainer.insertAdjacentElement("afterend", dragged);
+      }
+      console.log("Dropped non-repeatable token into existing token container:", dragged);
+      
+      // Sort the token containers by data-original-index
+      const sortedContainers = Array.from(target.querySelectorAll('.token-container')).sort((a, b) => {
+        return a.getAttribute('data-original-index') - b.getAttribute('data-original-index');
+      });
+      target.innerHTML = '';
+      sortedContainers.forEach(container => target.appendChild(container));
+    } else {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("token-container");
+      target.appendChild(newDiv);
+      newDiv.appendChild(dragged);
+      console.log("Dropped non-repeatable token into new token container:", dragged);
+      
+      // Sort the token containers by data-original-index
+      const sortedContainers = Array.from(target.querySelectorAll('.token-container')).sort((a, b) => {
+        return a.getAttribute('data-original-index') - b.getAttribute('data-original-index');
+      });
+      target.innerHTML = '';
+      sortedContainers.forEach(container => target.appendChild(container));
+    }
+  } else if (target.classList.contains("position")) {
     if (dragged.classList.contains("repeatable")) {
       const clone = dragged.cloneNode(true);
       const tokenName = prompt("Enter token name:", "");
       clone.querySelector(".token-name").innerText = tokenName;
-      if (dragged.parentElement.classList.contains("position") && dropTarget.classList.contains("position")) {
-        dragged.parentElement.removeChild(dragged);
-      }
-      dropTarget.appendChild(clone);
-    }else {
-      dragged.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-      if (dropTarget.querySelector(".token-container")) {
-        dropTarget.removeChild(dropTarget.querySelector(".token-container"));
-      }
-      dropTarget.appendChild(dragged);
+      target.appendChild(clone);
+      console.log("Dropped repeatable token into position:", clone);
+    } else {
+      target.appendChild(dragged);
+      console.log("Dropped non-repeatable token into position:", dragged);
     }
-    
-    const data = event.dataTransfer.getData("text");
-    const target = event.target;
-    console.log("drop event triggered");
-    console.log("Dropped token:", data);
-    console.log("Dropped into container:", target);
-    console.log("drop event triggered, target element:", event.target);
+  }
 
-    dragged.style.transform = "";
+  dragged.style.opacity = "1";
+  dragged = null;
+});
+
+
+const trashCan = document.querySelector("#trash-can");
+
+trashCan.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+
+trashCan.addEventListener("drop", function(event) {
+  event.preventDefault();
+  
+  const data = event.dataTransfer.getData("text");
+  const draggedElement = document.querySelector(`#${data}`);
+
+  if (draggedElement.parentElement.classList.contains("position") && draggedElement.classList.contains("repeatable")) {
+    draggedElement.remove();
+    console.log("Item has been deleted!");
+  } else {
+    console.log("Only repeatable tokens from position containers can be deleted!");
   }
 });
